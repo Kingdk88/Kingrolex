1e7fc476e83f5366a71f6218839b46cfcce4a799
Kingdk88 <143623116+Kingdk88@users.noreply.github.com>
2024-08-09 22:54:05 +0530

    Update config.py

--- config.py
+++ config.py
@@ -36,7 +36,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/Deepking88/Kingdk",
+    "https://github.com/Kingdk88/Kingrolex",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
